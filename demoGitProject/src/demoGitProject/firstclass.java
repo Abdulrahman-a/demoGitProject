4a324c3af8daa0e1d0e781f2a360e71cf27ed160
a <a@10.131.216.56>
2016-12-28 16:59:30 +0300

    test

--- demoGitProject/src/demoGitProject/firstclass.java
+++ demoGitProject/src/demoGitProject/firstclass.java
@@ -6,8 +6,8 @@ public class firstclass {
 		int x=5;
 		int y=7;
 		int z=8;
-		System.out.println("HELLLLLLLLLO");
-		System.out.println("please add two numbers");
+		System.out.println("wellcome");
+		System.out.println("helo");
 		System.out.println("Result is: "+ add(x,y));
 		//add(x,y);
 
